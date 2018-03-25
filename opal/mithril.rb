958b20dd83d9da82f63cc0c270e6cc92c5d68780
elphotono <elphotono@gmail.com>
2018-03-25 19:03:37 +0200

    rename virtual-node to mithril

--- opal/mithril.rb
+++ opal/mithril.rb
@@ -1,1 +1,11 @@
-require 'virtual_dom'
+require 'vendor/virtual-dom'
+require 'virtual_dom/dom'
+require 'virtual_dom/virtual_node'
+require 'virtual_dom/wrapper'
+require 'virtual_dom/hook'
+require 'virtual_dom/support'
+
+module VirtualDOM
+  include VirtualDOM::DOM
+  extend VirtualDOM::Wrapper
+end
